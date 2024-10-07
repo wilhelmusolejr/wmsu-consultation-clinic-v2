8560530bdfe5c5348afed3b2fa93f35215deb19f
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2024-10-07 14:04:18 +0800

    mobile view done home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 import wmsu_logo from "./assets/logo/Western_Mindanao_State_University.png";
 import he_logo from "./assets/logo/HE_Logo.png";
+import doctor from "./assets/doctor.png";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faPerson } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faEnvelope,
+  faPerson,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
@@ -34,7 +41,7 @@ function App() {
 
         {/* MAIN */}
         <div className="container flex flex-grow flex-col items-start justify-center p-5">
-          <h1 className="text-2xl font-bold uppercase">
+          <h1 className="text-3xl font-bold uppercase">
             Reach your instructor from anywhere!
           </h1>
           <p className="pb-5 pt-2 text-slate-600">
@@ -68,8 +75,8 @@ function App() {
       </div>
 
       {/* Service - steps */}
-      <div className="container px-5 py-10">
-        <h2 className="capitaliz py-10 text-center text-2xl">
+      <div className="container px-5 py-20">
+        <h2 className="capitaliz pb-10 text-center text-2xl">
           Quick Solutions For Scheduling with doctor
         </h2>
 
@@ -116,10 +123,10 @@ function App() {
       </div>
 
       {/* BMI */}
-      <div className="body-mass-index container flex min-h-80 flex-col justify-center p-5">
+      <div className="body-mass-index container flex min-h-80 flex-col justify-center px-5 py-10">
         <div className="">
-          <form action="">
-            <h3 className="my-5 text-center text-2xl font-medium text-slate-100">
+          <form action="" className="rounded-md bg-white p-2">
+            <h3 className="my-5 text-center text-2xl font-medium">
               Body Mass Index
             </h3>
 
@@ -127,7 +134,7 @@ function App() {
             <div className="mb-2">
               <label
                 htmlFor="height"
-                className="block text-sm font-medium leading-6 text-gray-300"
+                className="block text-sm font-medium leading-6 text-gray-700"
               >
                 Your height (cm)
               </label>
@@ -146,7 +153,7 @@ function App() {
             <div>
               <label
                 htmlFor="height"
-                className="block text-sm font-medium leading-6 text-gray-300"
+                className="block text-sm font-medium leading-6 text-gray-700"
               >
                 Your weight (lbs)
               </label>
@@ -170,6 +177,130 @@ function App() {
           </form>
         </div>
       </div>
+
+      {/* INSTRUCTORS */}
+      <div className="container px-5 py-20">
+        <h2 className="capitaliz pb-10 text-center text-2xl">
+          Meet Our Instructors
+        </h2>
+
+        <div className="flex flex-wrap gap-5">
+          {/* child */}
+          <div className="child rounded-lg bg-white p-5">
+            <div className="image-parent flex items-center justify-center rounded-lg bg-blue-200 px-3 pt-3">
+              <img src={doctor} alt="" />
+            </div>
+            <p className="mt-5 text-center">Rnd. Gregory Yames</p>
+          </div>
+          {/* child */}
+          <div className="child rounded-lg bg-white p-5">
+            <div className="image-parent flex items-center justify-center rounded-lg bg-blue-200 px-3 pt-3">
+              <img src={doctor} alt="" />
+            </div>
+            <p className="mt-5 text-center">Rnd. Gregory Yames</p>
+          </div>
+        </div>
+
+        <a
+          href="#"
+          className="btn my-5 mt-10 inline-block rounded-md border px-5 py-3 font-medium uppercase"
+        >
+          See more
+        </a>
+      </div>
+
+      {/* footer */}
+      <footer className="container px-5 py-20">
+        <div className="flex flex-col-reverse gap-10">
+          {/*  */}
+          <div className="">
+            <h2 className="mb-5 text-lg font-medium uppercase text-white">
+              Stay in Touch
+            </h2>
+
+            <div className="flex flex-col gap-3">
+              {/* child */}
+              <div className="flex items-center gap-3">
+                <div className="flex items-center justify-center rounded-md bg-white p-2">
+                  <FontAwesomeIcon icon={faPhone} />
+                </div>
+                <p className="text-gray-300">0997-297-6807</p>
+              </div>
+              {/* child */}
+              <div className="flex items-center gap-3">
+                <div className="flex items-center justify-center rounded-md bg-white p-2">
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </div>
+                <p className="text-gray-300">wmsuchedean@gmail.com</p>
+              </div>
+              {/* child */}
+              <div className="flex items-center gap-3">
+                <div className="flex items-center justify-center rounded-md bg-white p-2">
+                  <FontAwesomeIcon icon={faFacebook} />
+                </div>
+                <p className="text-gray-300">
+                  WMSU - College of Home Economics
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/*  */}
+          <div className="flex flex-col gap-5">
+            {/* child */}
+            <div className="">
+              <h2 className="mb-5 text-lg font-medium uppercase text-white">
+                Services
+              </h2>
+              <ul className="flex flex-col gap-2 capitalize text-gray-300">
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Doctors</a>
+                </li>
+                <li>
+                  <a href="#">Articles</a>
+                </li>
+              </ul>
+            </div>
+            {/* child */}
+            <div className="">
+              <h2 className="mb-5 text-lg font-medium uppercase text-white">
+                About{" "}
+              </h2>
+              <ul className="flex flex-col gap-2 capitalize text-gray-300">
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Doctors</a>
+                </li>
+                <li>
+                  <a href="#">Articles</a>
+                </li>
+              </ul>
+            </div>
+            {/* child */}
+            <div className="">
+              <h2 className="mb-5 text-lg font-medium uppercase text-white">
+                Legal{" "}
+              </h2>
+              <ul className="flex flex-col gap-2 capitalize text-gray-300">
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Doctors</a>
+                </li>
+                <li>
+                  <a href="#">Articles</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
